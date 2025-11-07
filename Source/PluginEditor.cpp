d654a4377bf535e9ae7f4d00239ef264b95f7abf
Hannes <hannes@localhost.com>
2025-11-07 09:45:13 +0100

    Add adaptive UI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,51 @@
 HelloWorldAudioProcessorEditor::HelloWorldAudioProcessorEditor (HelloWorldAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
+    setResizable(true, true);
+
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (500, 400);
+
+    //gainSlider.setVisible (true);
+    //addChildComponent (gainSlider);
+
+    addAndMakeVisible (gainSlider);
+    addAndMakeVisible (clickTextButton);
+    clickTextButton.setColour (TextButton::buttonColourId, Colours::purple);
+    clickTextButton.setColour (TextButton::textColourOffId, Colours::black);
+    clickTextButton.setColour (TextButton::textColourOnId, Colours::white);
+    clickTextButton.setClickingTogglesState(true);
+
+
+
+    addAndMakeVisible (selectComboBox);
+
+    // Nothing selected - Text
+    selectComboBox.setTextWhenNothingSelected( "Select Preset" );
+
+
+    // Add items
+    selectComboBox.addItem ("Cool Gain Slider", 1);
+    selectComboBox.addItem ("Groovy Bypass Toggle", 2);
+    selectComboBox.addItem ("Funky Clicky Textbutton", 3);
+    selectComboBox.addItem ("Awesome ComboBox", 4);
+
+    // Optionally set a default selection:
+    selectComboBox.setSelectedId (0);
+
+    addAndMakeVisible (bypassToggleButton);
+
+    //clickTextButton::TextButton ()
+
+    gainSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
+    //gainSlider.hideTextBox(true);
+    gainSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, false, 100, 10);
+
+
+
+
+
     DBG ("PluginEditor ()");
 }
 
@@ -32,14 +74,38 @@ void HelloWorldAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    // g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void HelloWorldAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-    
-    
-    
+
+//    Rectangle<int> bounds = getLocalBounds();
+    auto bounds = getLocalBounds ();
+    auto centre = bounds.getCentre ();
+    DBG ("Centre: " << centre.getX () << ", " << centre.getY ());
+
+    int width = bounds.getWidth ();
+    int height = bounds.getHeight ();
+
+    int xPos = centre.getX ();
+    int yPos = centre.getY ();
+
+    int smallestSide = jmin (width, height);
+    DBG ("smallestSide: " << smallestSide);
+
+    int sliderSide = smallestSide / 3;
+    DBG ("sliderSide: " << sliderSide);
+
+    // Reactivate again once the other elements are figured out!
+    gainSlider.setBounds(xPos - (sliderSide / 2), yPos - (sliderSide / 2), sliderSide, sliderSide);
+    //gainSlider.setBounds(50, 50, 100, 100);
+
+    bypassToggleButton.setBounds(25, JUCE_LIVE_CONSTANT(25), 25, 25);
+
+    selectComboBox.setBounds(xPos - 150, 25, 300, 25);
+    clickTextButton.setBounds(xPos - (width / 8), yPos + (sliderSide / 2) + (sliderSide / 10), width / 4, height / 6);
+
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,6 +28,24 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     HelloWorldAudioProcessor& audioProcessor;
+    //juce::Slider gainSlider;
+    Slider gainSlider;
+    ComboBox selectComboBox;
+    TextButton clickTextButton { "Start" } ;
+    ToggleButton bypassToggleButton;
+
+    // TODO:
+    // Erstellt ebenso eine juce::ComboBox, einen juce::TextButton, einen juce::ToggleButton.
+    // Fügt sie dem UI hinzu, findet ein halbwegs ansprechendes Layout :)
+    // Individualisiere einige Eigenschaften, Farben etc der einzelnen Components (Tutorium, wenn ihr einen Termin findet)
+
+    // Advanced:
+    // Erstellen eigener Components, bspw. eine Klasse, die einfach nur den gesamten Bereich rot färbt!
+
+
+    // Übersicht über mögliche juce Components findet ihr hier:
+    // https://docs.juce.com/master/classjuce_1_1Component.html
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HelloWorldAudioProcessorEditor)
 };
