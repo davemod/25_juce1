436d3917a7b4967c6c6177047b8590e480c18c97
Hannes <hannes@localhost.com>
2025-11-14 11:07:14 +0100

    Add RedBox class

---
+++ Source/RedBox.cpp
@@ -0,0 +1,9 @@
+//
+// Created by hannes on 11/14/25.
+//
+
+#include "RedBox.h"
+
+void RedBox::paint (Graphics& g) {
+    g.fillAll(Colours::red);
+}
---
+++ Source/RedBox.h
@@ -0,0 +1,13 @@
+//
+// Created by hannes on 11/14/25.
+//
+
+#pragma once
+
+#include <JuceHeader.h>
+//#include <juce_gui_basics/components/juce_Component.h>
+
+class RedBox : public juce::Component {
+public:
+    void paint (Graphics& g) override;
+};
