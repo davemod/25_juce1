70b6cd8049536aa428877ed5d960b6775e5e9931
David Hill <david.valentin.hill@gmail.com>
2025-11-21 10:38:33 +0100

    PresetMenuComponent, added todos

---
+++ Source/PresetMenuComponent.cpp
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    PresetMenuComponent.cpp
+    Created: 21 Nov 2025 9:42:48am
+    Author:  David Hill
+
+  ==============================================================================
+*/
+
+#include "PresetMenuComponent.h"
+
+PresetMenuComponent::PresetMenuComponent ()
+{
+    // addAndMakeVisible der ganze Buttons
+    DBG ("Preset Menu Component Created");
+}
+
+PresetMenuComponent::~PresetMenuComponent ()
+{
+    DBG ("Preset Menu Component Destroyed");
+}
+
+// 1. was gibt die funktion zurück? 2. zu welcher klasse gehört die funktion? 3. wie heißt die funktion? 4. was für argumente hat die funktion?
+void PresetMenuComponent::resized ()
+{
+    
+}
+
+void PresetMenuComponent::paint (Graphics& g)
+{
+    
+}
---
+++ Source/PresetMenuComponent.h
@@ -0,0 +1,43 @@
+/*
+  ==============================================================================
+
+    PresetMenuComponent.h
+    Created: 21 Nov 2025 9:42:48am
+    Author:  David Hill
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+// TODO 2
+// Stell die PresetMenuComponent fertig, sodass sie alle Buttons und das Label richtig anzeigt und positioniert. Füge sie PluginEditor hinzu und positioniere sie am oberen Rand.
+
+// TODO 3 - advanced
+// Beim klicken auf loadButton / saveButton soll sich ein juce::FileChooser öffnen.
+
+// TODO 4 - advanced
+// Schreibe und lese tatsächliche Dateien
+
+class PresetMenuComponent : public Component
+{
+public:
+    PresetMenuComponent ();
+    ~PresetMenuComponent () override;
+    
+    // 1 was gibt die funktion zurück? 2 wie heißt die funktion? 3 welche argumente bekommt die funktion übergeben? 4 Überschreibt die Funktion eine Funktion aus Component?
+    void resized () override;
+    void paint (juce::Graphics&) override;
+    
+private:
+    
+    // 1. WAS willst du haben? -> TextButton; 2. was soll der button für einen NAMEN haben? -> bspw. loadButton
+    TextButton loadButton;
+    TextButton saveButton;
+    TextButton nextButton;
+    TextButton previousButton;
+    
+    Label currentPresetLabel;
+};
