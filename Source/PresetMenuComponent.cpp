580e566722838260d1bdb595e3ca9e99b6912738
Hannes <hannes@localhost.com>
2025-11-21 09:45:55 +0100

    Add PresetMenuComponent

---
+++ Source/PresetMenuComponent.cpp
@@ -0,0 +1,6 @@
+//
+// Created by hannes on 11/21/25.
+//
+
+#include "PresetMenuComponent.h"
+
---
+++ Source/PresetMenuComponent.h
@@ -0,0 +1,12 @@
+//
+// Created by hannes on 11/21/25.
+//
+
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class PresetMenuComponent {
+};
+
